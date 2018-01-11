232b8e909633fca810abfc3f50eb5cf5022ceb50
rajdeepdasdeveloper <rajdeep.das.developer@gmail.com>
2018-01-11 15:50:06 +0530

    First Commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/* #################  MAIN JS  ################# */
+
+
+
+$(document).ready(function(){ // Ready Start
+
+	$(".carousel").slick({
+		dots: false,
+		autoplay: true,
+		autoplaySpeed: 5000,
+	    infinite: true,
+	    speed: 1500,
+	    fade: true,
+	    pauseOnHover: false,
+	    cssEase: 'linear'
+	});
+	
+}); // Ready End
+
+$(window).load(function(){ // Load Start
+	
+	
+}); // Load End
+
+$(window).resize(function(){ // Resize Start
+	
+	
+}); // Resize End
